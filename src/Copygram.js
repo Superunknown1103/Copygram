6671b577bd38fc22002486efdcbff98a48d7595d
Superunknown1103 <superunknown1103@gmail.com>
2019-01-11 08:25:24 -0600

    UI updates 1/11

--- src/Copygram.js
+++ src/Copygram.js
@@ -7,11 +7,22 @@ class Copygram extends Component {
         return(
         <View style= {{ flex: 1, width: 100 + '%', height: 100 + '%' }}>
             <View style={styles.tempNav} >
-
                 <Text>Copygram</Text>
+            </View>
 
-                </View>
-            <Image style={{ width: 100 + '%', height: 200 }} source={{uri: "https://lh3.googleusercontent.com/2OtUnlP_Kx1JGXcZcydNmW8PTDxCW26DJG3Gh-bQ4wTOtInmrX586vKiAqHde_fnCo81wY9qwDX_4nG6A3OD-4dhPA"}} />
+            <View style={styles.userBar}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Image 
+                source={{uri: "https://lh3.googleusercontent.com/bJAn6trTgn2sqzG46_BanWbrvi_sQ7tq6H55G46wy56zxZZvRaTdYi9GlDv8MYXBAUTFhR5t-DbhO4tnQtgdHQj9Hw"}} 
+                style={styles.userPic} />
+                <Text style={{ marginLeft: 10 }}>Bruce Goose</Text>
+            </View>
+            <View>
+                <Text>...</Text>
+            </View>
+            </View>
+            <Image style={{ width: 100 + '%', height: 200 }} 
+            source={{uri: "https://lh3.googleusercontent.com/2OtUnlP_Kx1JGXcZcydNmW8PTDxCW26DJG3Gh-bQ4wTOtInmrX586vKiAqHde_fnCo81wY9qwDX_4nG6A3OD-4dhPA"}} />
 
         </View>
         )
@@ -28,6 +39,22 @@ const styles = StyleSheet.create({
       borderBottomWidth: StyleSheet.hairlineWidth,
       justifyContent: "center",
       alignItems: "center"
+    },
+
+    userBar: {
+        width: 100+'%',
+        height: 50,
+        backgroundColor: "rgb(250, 250, 250)",
+        flexDirection: "row",
+        marginHorizontal: 10,
+        justifyContent: "stretch"
+    },
+
+    userPic: {
+        height: 40,
+        width: 40,
+        borderRadius: 20
+
     }
 
 })
